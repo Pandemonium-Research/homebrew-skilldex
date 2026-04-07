1fdeab5f9a3ced82325c5f8d5c6d9ce4cbb9e95a
Sampriti Saha <147638675+Sampriti2803@users.noreply.github.com>
2026-04-08 00:14:25 +0530

    Update skilldex-cli to version 1.0.0

--- Formula/skilldex-cli.rb
+++ Formula/skilldex-cli.rb
@@ -1,8 +1,8 @@
 class SkilldexCli < Formula
   desc "Package manager and registry for Claude skill packages"
   homepage "https://github.com/Pandemonium-Research/Skilldex"
-  url "https://registry.npmjs.org/skilldex-cli/-/skilldex-cli-0.1.0.tgz"
-  sha256 "b1aaada5bb6a13df54eb838c0c9c6e03dcd44422d32718ff54291d781c172c52"
+  url "https://registry.npmjs.org/skilldex-cli/-/skilldex-cli-1.0.0.tgz"
+  sha256 "00077b607608f6d5215dc6df814777a090c5aebf7ff0e68e549bf9cf095e32c8"
   license "MIT"
 
   depends_on "node"
@@ -13,6 +13,6 @@ class SkilldexCli < Formula
   end
 
   test do
-    assert_match "0.1.0", shell_output("#{bin}/skillpm --version")
+    assert_match "1.0.0", shell_output("#{bin}/skillpm --version")
   end
 end
